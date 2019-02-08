6b2ac92cefae7fe4cd1829f5c465b9e86cdc9617
MATTHEW <matthew.heath@hotmail.com>
2019-02-08 14:06:23 +0000

    Add componentDidMount and componentDidUpdate methods

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import axios from 'axios';
 
 class ResourceList extends React.Component {
+    state = {resources: []}
+
+    async componentDidMount() {
+        const response = await axios.get(`https://api.spacexdata.com/v3/launches/${this.props.resource}`);
+        this.setState({resources: response.data});
+    }
+
+    async componentDidUpdate(prevProps) {
+        if (prevProps.resource !== this.props.resource) {
+            const response = await axios.get(`https://api.spacexdata.com/v3/launches/${this.props.resource}`);
+            this.setState({resources: response.data});
+        }
+    }
+
     render() {
         return (
             <>
-                {this.props.resource}
+                {this.state.resources.length}
             </>
         );
     }
